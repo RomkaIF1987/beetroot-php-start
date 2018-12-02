031d1c97a54d45e121d42173eaa23cfe7c1fa9ca
Roman Zhyliak <romka.if87@gmail.com>
2018-12-02 21:57:18 +0200

    Added name verification

--- controllers/save-name.php
+++ controllers/save-name.php
@@ -1,10 +1,23 @@
 <?php
-$newName = $_POST['name'];
-$app['database']->insertName('clients',$newName);
 
-if ($newName === false){
-    echo 'Not string';
+$newName = $_POST['name'];
+$newName = Request::clean_input($newName);
+if (!empty($newName)) {
+    if (Request::check_length($newName,2,20)) {
+        echo "Дякуємо за повідомлення";
+//        echo "$newName";
+    } else {
+        echo "Введені некоректні дані";
+        return false;
+    }
+} else {
+    echo "Заповніть пусті поля";
+    return false;
 }
+
+
+$app['database']->insertName('clients', $newName);
+
 
 
 
--- core/Request.php
+++ core/Request.php
@@ -1,12 +1,31 @@
 <?php
- class Request
- {
+
+class Request
+{
     public static function uri()
     {
-        return trim($_SERVER['REQUEST_URI'],'/');
+        return trim($_SERVER['REQUEST_URI'], '/');
     }
+
     public static function method()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
- }
+
+    public static function clean_input($value)
+    {
+        $value = trim($value);
+        $value = stripslashes($value);
+        $value = strip_tags($value);
+        $value = htmlspecialchars($value);
+
+        return $value;
+    }
+
+    public static function check_length($value, $min, $max)
+    {
+        $result = (mb_strlen($value) < $min || mb_strlen($value) > $max);
+        return !$result;
+    }
+
+}
