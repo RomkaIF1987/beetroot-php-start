8d1d14a7e896b093838b3033cab1be867e7f4564
Roman Zhyliak <romka.if87@gmail.com>
2018-12-05 21:19:30 +0200

    added checked input characters

--- controllers/save-name.php
+++ controllers/save-name.php
@@ -5,11 +5,11 @@ $newName = Verification::cleanInput($newName);
 if (!empty($newName)) {
     if (Verification::checkLength($newName, 2, 20)) {
     } else {
-        echo "Введені некоректні дані <p><a href='ask-name'>ПОВЕРНУТИСЯ НАЗАД</a></p>";
+        echo "<h1 style='color: red'>Введені некоректні дані</h1> <p><a href='ask-name'>ПОВЕРНУТИСЯ НАЗАД</a></p>";
         return false;
     }
 } else {
-    echo "Заповніть пусті поля <p><a href='ask-name'>ПОВЕРНУТИСЯ НАЗАД</a></p>";
+    echo "<h1 style='color: red'>Заповніть пусті поля</h1> <p><a href='ask-name'>ПОВЕРНУТИСЯ НАЗАД</a></p>";
     return false;
 }
 
